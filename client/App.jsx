bb23ab5bcbd0fecc8693ef14121ee13cb96eec18
b-hash <brianmhayashi@gmail.com>
2020-09-12 13:57:00 -0700

    (updated) App.jsx with functional component

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
+// import { render } from 'react-dom';
+import Main from './containers/MainContainer.jsx';
 
+const App = () => (
+  <div>
+    Hello from App.jsx!
+    <Main />
+  </div>
+);
+
+export default App;
