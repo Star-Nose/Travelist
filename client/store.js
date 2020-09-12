046a4f51e251406aed88b9beea3e65d382319493
b-hash <brianmhayashi@gmail.com>
2020-09-12 14:17:04 -0700

    (added) store and reducers

---
+++ client/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
+import combinedReducers from './reducers/index.js';
+
+// store will hold our reducers and always reflect the one source of truth
+const store = createStore(
+  combinedReducers,
+  // composeWithDevTools()
+);
+
+export default store;
