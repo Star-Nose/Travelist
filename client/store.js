3479756db1d845bca7399a0c6bfc33ddd53decb5
b-hash <brianmhayashi@gmail.com>
2020-09-12 15:43:52 -0700

    (added) redux devtools extension package

--- client/store.js
+++ client/store.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import combinedReducers from './reducers/index';
 
 // store will hold our reducers and always reflect the one source of truth
 const store = createStore(
   combinedReducers,
-  // composeWithDevTools()
+  composeWithDevTools(),
 );
 
 export default store;
