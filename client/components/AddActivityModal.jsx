66ccd58cd91c3580eef16449543cb93ee180b101
mig824 <migmic93@gmail.com>
2020-09-14 16:55:03 -0700

    (add) created new modal form component

---
+++ client/components/AddActivityModal.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+const AddActivityModal = ({
+  show,
+  onHide,
+  description,
+  notes,
+  address,
+  link,
+  handleFormInput,
+  handleFormSubmit,
+}) => {
+  const [inputValues, setInputValues] = useState([]);
+
+  return (
+    <Modal size="lg" centered show={show}>
+      <Modal.Header>
+        <Modal.Title>Add Activity</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleFormSubmit({ description, notes, address, link });
+          }}
+        >
+          <Form.Group controlId="activityDescription">
+            <Form.Label>Activity Description</Form.Label>
+            <Form.Control
+              type="text"
+              name="description"
+              onChange={(e) => handleFormInput(e.target)}
+            />
+          </Form.Group>
+          <Form.Group controlId="activityNotes">
+            <Form.Label>Notes</Form.Label>
+            <Form.Control
+              name="notes"
+              as="textarea"
+              onChange={(e) => handleFormInput(e.target)}
+            />
+          </Form.Group>
+          <Form.Group controlId="activityAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              name="address"
+              type="text"
+              onChange={(e) => handleFormInput(e.target)}
+            />
+          </Form.Group>
+          <Form.Group controlId="activityLink">
+            <Form.Label>Link a URL to relevant info</Form.Label>
+            <Form.Control
+              name="link"
+              type="url"
+              onChange={(e) => handleFormInput(e.target)}
+            />
+          </Form.Group>
+
+          <Button variant="danger" type="submit" className="mt-4">
+            Add Activity
+          </Button>
+          <Button
+            variant="outline-danger"
+            className="mt-4 ml-2"
+            onClick={() => onHide()}
+          >
+            Cancel
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default AddActivityModal;
