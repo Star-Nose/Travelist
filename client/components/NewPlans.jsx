33c4eb2335030ec196bb7387e3af291309df051f
SanjayLavingia <64515025+SanjayLavingia@users.noreply.github.com>
2020-09-14 13:25:11 -0700

    location/trip cards rendering in sidebar

--- client/components/NewPlans.jsx
+++ client/components/NewPlans.jsx
@@ -3,23 +3,40 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 const NewPlans = (props) => {
-  const { newLocationInput } = props;
+  const {
+    newLocationInput, country, location, addNewPlans,
+  } = props;
   return (
     <div id="new-plans-container">
-      <Form className="itinerary-form">
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addNewPlans({ location, country });
+        }}
+        className="itinerary-form"
+      >
         <Form.Group controlId="formLocation">
           <Form.Label>New Location:</Form.Label>
-          <Form.Control type="text" placeholder="" onChange={newLocationInput} />
+          <Form.Control
+            name="newLocation"
+            type="text"
+            placeholder=""
+            onChange={(e) => newLocationInput(e.target)}
+          />
         </Form.Group>
         <Form.Group controlId="formCountry">
           <Form.Label>Country:</Form.Label>
-          <Form.Control type="text" placeholder="" />
+          <Form.Control
+            name="country"
+            type="text"
+            placeholder=""
+            onChange={(e) => newLocationInput(e.target)}
+          />
         </Form.Group>
-        <Button variant="secondary" id="new-plans">Make new plans</Button>
+        <Button type="submit" variant="secondary" id="new-plans">Make new plans</Button>
       </Form>
     </div>
-)
-  };
+  );
+};
 
 export default NewPlans;
-
--- client/containers/SidebarContainer.jsx
+++ client/containers/SidebarContainer.jsx
@@ -1,42 +1,49 @@
 /* eslint-disable import/extensions */
-//  eslint-disable react/prop-types 
+//  eslint-disable react/prop-types
 import React from 'react';
 import { connect } from 'react-redux';
 import SidebarCard from '../components/SidebarCard.jsx';
-import NewPlans from '../components/NewPlans.jsx'
+import NewPlans from '../components/NewPlans.jsx';
 
 import * as actions from '../actions/actions.js';
 
 const mapDispatchToProps = (dispatch) => ({
   getActivities: (tripId) => dispatch(actions.getActivities(tripId)),
   newLocationInput: (e) => dispatch(actions.newLocationInput(e)),
+  addNewPlans: (e) => dispatch(actions.newPlans(e)),
 });
 
 const mapStateToProps = (state) => ({
-  count: state.trips.count,
-  location: 'Hollywood, CA',
-  country: 'USA',
-  newLocation: state.trips.newLocation,
+  country: state.form.newPlans.country,
+  newLocation: state.form.newPlans.newLocation,
+  trips: state.trips.trips,
 });
 
 // long rectangle that renders cards based on
 const SidebarContainer = (props) => {
   // get request to server (server will query db)
-  const { getActivities, location, country, newPlans, newLocationInput } = props;
-  const cardsArray = [];
+  const {
+    getActivities, addNewPlans, newLocationInput, country, newLocation, trips,
+  } = props;
   // loop through locations in db and and create <SidebarCard /> for each
   return (
     <div className="side-bar">
       <h5 id="itineraries">Itineraries</h5>
       <NewPlans
+        addNewPlans={addNewPlans}
+        country={country}
+        location={newLocation}
         newLocationInput={newLocationInput}
       />
-      <SidebarCard
-        tripId={1}
-        getActivities={getActivities}
-        location={location}
-        country={country}
-      />
+      {trips.map((trip, i) => (
+        <SidebarCard
+          key={`tripId${i}`}
+          tripId={`tripId${i}`}
+          getActivities={getActivities}
+          country={country}
+          location={newLocation}
+        />
+      ))}
     </div>
   );
 };
