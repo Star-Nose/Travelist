34a6006c80f255b387b821420b350b727db1c4dd
mig824 <migmic93@gmail.com>
2020-09-13 19:10:16 -0700

    (update) initial state in form reducer, combined formreducer with tripsreducer

--- client/reducers/formReducer.js
+++ client/reducers/formReducer.js
@@ -1,11 +1,13 @@
 import * as types from '../constants/actionTypes';
+import axios from 'axios';
 
 const initialState = {
   signUp: {
-    name: '',
+    firstName: '',
     email: '',
     username: '',
     password: '',
+    confirmedPassword: '',
   },
   logIn: {
     username: '',
@@ -15,15 +17,39 @@ const initialState = {
 
 const formReducer = (state = initialState, action) => {
   switch (action.type) {
-    case types.SIGN_UP_SUBMIT:
+    case types.SUBMIT_SIGNUP_FORM:
+      console.log(action.payload);
+      axios
+        .post('/signup', action.payload)
+        .then((response) => {
+          console.log('in reducer --> ', response);
+        })
+        .catch((err) => console.log('err in reducer: ', err));
+
       return {
         ...state,
         signUp: {
+          ...state.signUp,
           ...action.payload,
         },
       };
+    case types.SIGNUP_FORM_INPUT: {
+      const { name, value } = action.payload;
+      // console.log('name: ', name);
+      // console.log('value: ', value);
+      // console.log('state: ', state);
+      return {
+        ...state,
+        signUp: {
+          ...state.signUp,
+          [name]: value,
+        },
+      };
+    }
 
     default:
       return state;
   }
 };
+
+export default formReducer;
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import travelReducer from './travelReducer';
+import formReducer from './formReducer';
 
 const combinedReducers = combineReducers({
   trips: travelReducer,
+  form: formReducer,
 });
 
 export default combinedReducers;
