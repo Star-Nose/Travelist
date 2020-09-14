d2accb8b8221662f44338378b12dd8b8937809b3
mig824 <migmic93@gmail.com>
2020-09-14 16:53:45 -0700

    (add) imported modal component into container

--- client/containers/ActivitiesContainer.jsx
+++ client/containers/ActivitiesContainer.jsx
@@ -1,26 +1,58 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import Activity from '../components/Activity.jsx';
+import ActivityFormModal from '../components/AddActivityModal.jsx';
 import * as actions from '../actions/actions';
 
 const mapDispatchToProps = (dispatch) => ({
-  addCount: () => dispatch(actions.addCount()),
+  handleFormInput: (newState) => dispatch(actions.activityFormInput(newState)),
+  handleFormSubmit: (newState) =>
+    dispatch(actions.activityFormSubmit(newState)),
 });
 
 const mapStateToProps = (state) => ({
-  count: state.trips.count,
+  description: state.form.newActivity.description,
+  notes: state.form.newActivity.notes,
+  address: state.form.newActivity.address,
+  link: state.form.newActivity.link,
+  completed: state.form.newActivity.completed,
 });
 
-const ActivitiesContainer = (props) => (
-  <div>
-    <div>Activities Container</div>
-    <Button>Add Activity</Button>
-    <div>
-      <Activity />
-    </div>
-  </div>
-);
+const ActivitiesContainer = (props) => {
+  const [showModal, setShowModal] = useState(false);
+  const {
+    description,
+    notes,
+    address,
+    link,
+    handleFormInput,
+    handleFormSubmit,
+  } = props;
 
-export default connect(mapStateToProps, mapDispatchToProps)(ActivitiesContainer);
+  return (
+    <div>
+      <ActivityFormModal
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        description={description}
+        notes={notes}
+        address={address}
+        link={link}
+        handleFormInput={handleFormInput}
+        handleFormSubmit={handleFormSubmit}
+      />
+      <div>Activities Container</div>
+      <Button onClick={() => setShowModal(true)}>Add Activity</Button>
+      <div>
+        <Activity />
+      </div>
+    </div>
+  );
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ActivitiesContainer);
